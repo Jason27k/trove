a19967455a3b96f102851690e2e5769298762b4c
Jason27k <68726620+Jason27k@users.noreply.github.com>
2024-08-04 11:21:36 -0700

    Initial Commit

---
+++ anime-v2/anime_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  anime_v2App.swift
+//  anime-v2
+//
+//  Created by Jason Morales on 8/4/24.
+//
+
+import SwiftUI
+
+@main
+struct anime_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
